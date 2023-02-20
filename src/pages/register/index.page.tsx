b3110182e050ece56f74a9d60c8ba3a427628ed6
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-20 13:11:39 -0300

    Register page

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,31 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Form, Header } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as={'strong'}>Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as={'form'}>
+        <label>
+          <Text size={'sm'}>Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+        </label>
+        <label>
+          <Text size={'sm'}>Nome completo</Text>
+          <TextInput placeholder="Seu Nome" />
+        </label>
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
