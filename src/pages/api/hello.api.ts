4b797b2dcce8b40e30021b6ece72e08fd8570aa7
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-20 13:55:49 -0300

    Prisma config init

---
+++ src/pages/api/hello.api.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+type Data = {}
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  res.status(200).json({ name: 'John Doe' })
+}
