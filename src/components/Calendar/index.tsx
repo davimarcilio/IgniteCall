5aae80ef8a83ef35de40698072a1a0c257b0b4f1
Davi Marcilio <davimarcilio.js@gmail.com>
2023-07-02 11:47:46 -0300

    query fixes

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -65,7 +66,7 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
         {
           params: {
             year: currentDate.get('year'),
-            month: currentDate.get('month'),
+            month: currentDate.get('month') + 1,
           },
         },
       )
@@ -120,7 +121,10 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
 
         return {
           date,
-          disabled: date.endOf('day').isBefore(new Date()) || dayHasBlocked,
+          disabled:
+            date.endOf('day').isBefore(new Date()) ||
+            dayHasBlocked ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -42,14 +42,27 @@ export default async function handle(
     })
   })
 
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT * 
+  const blockedDatesRaw: Array<{
+    date: number
+  }> = await prisma.$queryRaw`
+    SELECT 
+    EXTRACT(DAY FROM S.date) as date,
+    COUNT(S.date) as amount,
+    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes)/60) AS size
     FROM schedulings S
+    LEFT JOIN user_time_intervals UTI
+      on UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
     WHERE S.user_id = ${user.id}
       AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`} 
+    GROUP BY EXTRACT(DAY FROM S.date),
+    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes)/60)
+    HAVING amount >= size
   `
+
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
 
   return res.json({
     blockedWeekDays,
+    blockedDates,
   })
 }
