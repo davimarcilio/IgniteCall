2a29e51afd29ed44960d8fbba6c12fdd42a23481
Davi Marcilio <davimarcilio.js@gmail.com>
2023-07-02 13:03:59 -0300

    Add in google calendar

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,5 +1,7 @@
+import { getGoogleOAuthToken } from '@/lib/google'
 import { prisma } from '@/lib/prisma'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
 
@@ -55,7 +57,7 @@ export default async function handle(
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       date: schedulingDate.toDate(),
       email,
@@ -64,5 +66,40 @@ export default async function handle(
       user_id: user.id,
     },
   })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [
+        {
+          email,
+          displayName: name,
+        },
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
+    },
+  })
+
   return res.status(201).end()
 }
